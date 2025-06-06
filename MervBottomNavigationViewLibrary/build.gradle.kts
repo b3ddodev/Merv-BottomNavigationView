b94bdc28bb1f3e80d6c56ce56b5cc142c760307f
b3ddodev <withb3ddodev@gmail.com>
2025-06-06 22:47:48 +0300

    ⬆️ Update: Merv BottomNavigationView Library Module

--- MervBottomNavigationViewLibrary/build.gradle.kts
+++ MervBottomNavigationViewLibrary/build.gradle.kts
@@ -48,7 +48,7 @@ publishing {
         create("release", MavenPublication::class) {
             groupId = "com.github.b3ddodev"
             artifactId = "Merv-BottomNavigationView"
-            version = "1.1.1-0103-rls"
+            version = "1.1.2-0103-rls"
 
             afterEvaluate {
                 from(components["release"])
